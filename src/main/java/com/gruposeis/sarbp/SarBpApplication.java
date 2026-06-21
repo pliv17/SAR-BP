ad2514822309c2dbf855c2d57087f70b2d37f33a
pliv17 <gokulegana.17@gmail.com>
2026-06-21 03:20:23 -0500

    configuración básica del proyecto. conexión con supabase

---
+++ src/main/java/com/gruposeis/sarbp/SarBpApplication.java
@@ -0,0 +1,13 @@
+package com.gruposeis.sarbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SarBpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SarBpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gruposeis/sarbp/SarBpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gruposeis.sarbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SarBpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
